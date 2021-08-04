07a055b0ecaaff009555ebad8d1cc6b04f866885
vishnuvardhan860 <vishnuvardhan860@gmail.com>
2021-08-04 21:45:20 +0530

    Added:Adding_Valid_Email

---
+++ ValidEMail.java
@@ -0,0 +1,81 @@
+package com.userregistration;
+
+import java.util.Scanner;
+
+public class ValidEMail {
+
+        private static final String NamePattern = "^[A-Z][a-zA-Z]{2,}$";
+        private static final String EmailPattern = "^[0-9a-zA-Z]+([.\\-+]?[a-zA-Z0-9]+)?\\@[0-9a-zA-Z]+.[a-zA-Z]{2,4}(.[a-zA-Z]{2,})*$";
+        private static final String PhonenumberPattern ="^([\\+]?91)?[6-9]{1}[0-9]{9}$";
+        private static final String PasswordPattern ="^.{8,}$";
+        Scanner scanner = new Scanner(System.in);
+        public void FirstnameValidator()
+        {
+            System.out.println("Enter First Name");
+            String input = scanner.next();
+            if (input.matches(NamePattern)) {
+                System.out.println("first name sucessfully entered");
+            }
+            else {
+                System.out.println("*Invalid First nametry again*");
+                FirstnameValidator();
+            }
+        }
+        public void LastnameValidator() {
+            System.out.println("Enter the last name:");
+            String input = scanner.next();
+            if (input.matches(NamePattern)) {
+                System.out.println("last name sucessfully entered");
+            }
+            else {
+                System.out.println("*Invalid last nametry again*");
+                LastnameValidator();
+            }
+        }
+        public void EmailValidator() {
+            System.out.println("Enter the email id :");
+            String input = scanner.next();
+            if (input.matches(EmailPattern)) {
+                System.out.println("email id sucessfully entered");
+            }
+            else {
+                System.out.println("*Invalid email idtry again*");
+                EmailValidator();
+            }
+        }
+        public void PhoneNumberValidator() {
+            System.out.println("Enter the phone Number:");
+            String input = scanner.next();
+            if (input.matches(PhonenumberPattern)) {
+                System.out.println("phone number sucessfully entered");
+            }
+            else {
+                System.out.println("*Invalid phone numbertry again*");
+                PhoneNumberValidator();
+            }
+        }
+        public void PasswordValidator() {
+            System.out.println("Enter the password:");
+            String input = scanner.next();
+            if (input.matches(PasswordPattern)) {
+                System.out.println("password sucessfully entered");
+            }
+            else {
+                System.out.println("*Invalid passwordtry again*");
+                PasswordValidator();
+            }
+        }
+        public static void main(String[] args) {
+            System.out.println("Welcome to user registration");
+            ValidEMail ve = new ValidEMail();
+            ve.FirstnameValidator();
+            ve.LastnameValidator();
+            //getting Email results
+            ve.EmailValidator();
+            
+        }
+
+
+
+
+}
